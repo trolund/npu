af44c8193a2024f057216c7c1e8db0e883070808
TroelsLund <tlu@cvation.com>
2025-03-25 14:42:42 +0100

    more

--- src/NPU.ApiTests/GetNpuByIdTests.cs
+++ src/NPU.ApiTests/GetNpuByIdTests.cs
@@ -1,7 +1,4 @@
 using System.Net;
-using System.Text;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
 using NPU.ApiTests.TestHelpers;
 using NPU.Infrastructure.Dtos;
 
@@ -12,13 +9,13 @@ public class GetNpuByIdTests(WebApiApplication factory) : TestBase(factory)
     [Fact]
     public async Task GIVEN_AUserInTheDB_WHEN_ANpuIsGetById_THEN_TheUserIsReturned_Correctly()
     {
-        // Arrange
+        // GIVEN
         var npu = DatabaseSeeding.Npus.First();
         
-        // Act
+        // WHEN
         var (_, data) = await GetAndDeserialize<NpuResponse>(Routes.Npus.Get(npu.Id));
 
-        // Assert
+        // THEN
         Assert.NotNull(data);
         Assert.Equal(npu.Id, data.Id);
         Assert.Equal(npu.Name, data.Name);
@@ -29,20 +26,20 @@ public class GetNpuByIdTests(WebApiApplication factory) : TestBase(factory)
     [Fact]
     public async Task GIVEN_AUserIsNotInTheDB_WHEN_ANpuIsGetById_THEN_ThenA404IsReturned()
     {
-        // Arrange
+        // GIVEN
         const string npuId = "00000000-0000-0000-0000-000000000000";
         
-        // Act
+        // WHEN
         var result = await ApiClient.GetAsync(Routes.Npus.Get(npuId));
 
-        // Assert
+        // THEN
         Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
     }
     
     [Fact]
     public async Task GIVEN_InvalidCreateNpuRequest_WHEN_PostNpu_THEN_400IsReturned()
     {
-        // Arrange
+        // GIVEN
         var invalidRequest = new CreateNpuRequest
         {
             Name = "", // Invalid: Required field is empty
@@ -50,10 +47,10 @@ public class GetNpuByIdTests(WebApiApplication factory) : TestBase(factory)
             Images = [] 
         };
         
-        // Act
+        // WHEN
         var (httpMsg, data) = await PostAndSerialize<CreateNpuRequest, dynamic>(Routes.Npus.Create, invalidRequest);
 
-        // Assert
+        // THEN
         Assert.Equal(HttpStatusCode.BadRequest, httpMsg.StatusCode);
     }
 }
